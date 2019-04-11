1d2831671295afbd507380dd2f86f0109bb891be
irocho <irocho.profe@gmail.com>
2019-04-11 10:47:38 +0200

    enlaces co wiki

---
+++ nodejs/servidor_simple.js
@@ -0,0 +1,22 @@
+//Servidor mínimo en Node
+//
+// explicado paseniño no wiki:
+// https://github.com/irocho/javascriptAmpliado/wiki/Node.js/
+//
+//
+var http = require("http");
+
+http.createServer(function(request, response) {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("Servidor funcionando");
+      response.end();
+}).listen(8888);
+
+
+
+// executamos no terminal
+// node servidor_simple.js
+//
+// visitamos no navegador
+//
+// http://localhost:8888
