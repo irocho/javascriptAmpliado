046cd57d596277f2a5708d530400f95389c96493
irocho <irocho.profe@gmail.com>
2019-04-12 12:02:08 +0200

    xusto antes semana santa

---
+++ nodejs/fich_ver.js
@@ -0,0 +1,13 @@
+//
+// Mostra o contido dun ficheiro
+//
+
+
+fs = require('fs');
+
+fs.readFile('/etc/hosts', 'utf8', function(err, data){
+    if (err){
+        return console.log(err);
+    }
+    console.log(data);
+});
