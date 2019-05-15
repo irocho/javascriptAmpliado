82b60beecfd21ecea988f7e82d16f592a5f02f43
irocho <irocho.profe@gmail.com>
2019-05-15 13:57:49 +0200

    ng-click outra vez

--- angularjs/limitar_texto/limite.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('calculadoraCtrl', function($scope) {
-    $scope.x1 = 10;
-    $scope.x2 = 10;
-    $scope.total = function() {
-        return $scope.x1 + $scope.x2;
-    };
-});
-
